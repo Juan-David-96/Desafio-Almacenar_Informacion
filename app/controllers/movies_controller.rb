36e678c315a7145b36367b385155bc2231887ed6
Juan_D <jdvc96@gmail.com>
2024-10-01 21:04:03 -0300

    Primer commit Movies

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,25 @@
+class MoviesController < ApplicationController
+  def index
+    @movie_all = movie.all
+  end
+
+  def create
+    @movie = movie.new(movies_params)
+    if @movie.save
+    redirect_to movies_index_path
+    else
+    render :new
+    end
+  end
+    
+
+  def new
+    @movie=Movie.new
+  end
+
+
+  def movies_params
+    params.require(:movie).permit(:name, :synopsis, :director)
+  end
+end
+  
